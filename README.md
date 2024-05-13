8fc2c403afe9e355aeae734aa88d2497f388d123
Mizanur Rahman <mizanurrahman615@gmail.com>
2024-05-13 11:51:41 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+<h1>Bank Management System</h1>
+<ul>
+<li>1. Clone the project,</li>
+<li>2. go to project directory by command cd /project</li>
+<li>3. install vendor folder by running command composer install</li>
+<li>4. create a database as you want</li>
+<li>5. set the database name to .env file</li>
+<li>6. run the command php artisan optimize</li>
+<li>7. then run php artisan migrate for running database migration.</li>
+<li>8. finally run php artisan serve to run the project and enjoy.</li>
+</ul>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <div class="table-responsive">
+                        <table class="table table-sm text-center">
+
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
